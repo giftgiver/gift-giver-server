4c285db1350589cc4493fe3988cfa3473ac15e3b
Mychael Walton <mychael.walton@nike.com>
2018-11-20 21:01:45 -0800

    localhost to build commands

--- src/index.js
+++ src/index.js
@@ -84,7 +84,20 @@ const resolvers = {
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: async ({ req }) => {
+    console.log('Context Function');
+    //auth stuff happens here
+    // if (!req || !req.headers) {
+    //   return;
+    // }
+    // const token = req.headers.authorization || '';
+    // const checkToken = await userController.findOrCreateUser(token);
+    // if (!checkToken.hasOwnProperty('authorized')) {
+    //   return { user: checkToken, authorized: true };
+    // }
+    // return checkToken;
+  }
 });
 
 const start = async port => {
