6703659c45ed621e9cdbb40d634301e80badf5c0
Mychael Walton <mychael.walton@crmblackbox.com>
2018-11-21 19:10:33 -0800

    jwt auth validation working, protecting getUser query

--- src/modules/auth/auth.js
+++ src/modules/auth/auth.js
@@ -1,22 +1,84 @@
+const {
+  ForbiddenError,
+  UserInputError,
+  AuthenticationError
+} = require('apollo-server');
 const jwt = require('jsonwebtoken');
 const path = require('path');
 const fs = require('fs');
+const log = require('pino')();
+const _ = require('lodash');
+const AUTHED_OPERATION = ['getUser'];
 
-const privateKey = fs.readFileSync(
-  path.resolve(__dirname, '../../../keys/gift-giver-server.private')
-);
-const publicKey = fs.readFileSync(
-  path.resolve(__dirname, '../../../keys/gift-giver-server.pub')
-);
+let privateKey;
+let publicKey;
 
-const getSignedJwt = email => {
-  const token = jwt.sign({ email: email }, privateKey, { algorithm: 'RS256' });
+const JWT_VERIFY_ERROR = 'JWT Authentication Failed';
+
+const loadkeys = () => {
+  try {
+    // PRIVATE KEYS TODO: migrate this to like kms? key rotates are neat
+    privateKey = fs.readFileSync(
+      path.resolve(__dirname, '../../../keys/private.pem')
+    );
+    publicKey = fs.readFileSync(
+      path.resolve(__dirname, '../../../keys/public.pem')
+    );
+  } catch (error) {
+    log.error(error);
+    throw new Error(error);
+  }
+};
+
+// Auth middleware, checks each for auth before performing query.
+const context = async ({ req }) => {
+  const token = req.headers.authorization || '';
+  const authed = isAuthed(req);
+
+  // Let Non-Authed endpoints through
+  if (!authed) {
+    return req;
+  }
+  // Return Auth error for authed missing tokens on authed queries
+  if (authed && !token) {
+    throw new AuthenticationError(JWT_VERIFY_ERROR);
+  }
+
+  if (authed && !verifyJwt(token)) {
+    throw new AuthenticationError();
+  }
+  return req;
+};
+
+const isAuthed = req => {
+  const body = req.body || '';
+  const operationName = body.operationName || '';
+
+  if (operationName) {
+    return _.includes(AUTHED_OPERATION, operationName);
+  } else {
+    throw new UserInputError(
+      'Missing Operation Name, shorthand queries not supported.'
+    );
+  }
+};
+
+const getSignedJwt = async email => {
+  const token = await jwt.sign({ email: email }, privateKey, {
+    algorithm: 'RS256'
+  });
   return token;
 };
 
-const verifyJwt = jwt => {
-  const decodedJwt = jwt.verify(jwt, publicKey);
-  return decodedJwt;
+const verifyJwt = async token => {
+  const decodedJwt = await jwt.verify(token, publicKey);
+
+  if (!decodedJwt) {
+    throw new AuthenticationError(JWT_VERIFY_ERROR);
+  } else {
+    return decodedJwt;
+  }
 };
 
-module.exports = { getSignedJwt, verifyJwt };
+loadkeys();
+module.exports = { context, getSignedJwt, verifyJwt };
--- src/server.js
+++ src/server.js
@@ -8,20 +8,7 @@ const typeDefs = require('./graphql/types/typeDefs');
 const resolvers = require('./graphql/resolvers/resolvers');
 const _ = require('lodash');
 const log = require('pino')();
-const authMiddleware = require('./modules/middleware/authMiddleware');
-
-const AUTHED_ENDPOINTS = ['getUser'];
-
-const isAuthed = req => {
-  const body = req.body || '';
-  const operationName = body.operationName || '';
-
-  if (operationName) {
-    return _.includes(AUTHED_ENDPOINTS, operationName);
-  } else {
-    throw new UserInputError('Invalid body, check query.');
-  }
-};
+const auth = require('../src/modules/auth/auth');
 
 const server = new ApolloServer({
   typeDefs,
@@ -30,37 +17,7 @@ const server = new ApolloServer({
     log.error(error);
     return error;
   },
-  subscriptions: {
-    onConnect: async (connectionParams, webSocket, context) => {
-      console.log('on connect?');
-    },
-    onDisconnect: (webSocket, context) => {
-      console.log('on disconnect?');
-    }
-  },
-  context: async ({ req }) => {
-    const token = req.headers.authorization || '';
-
-    if (isAuthed(req) && !token) {
-      throw new AuthenticationError('Missing Auth Header');
-    }
-
-    // if(_.hasIn(req, ''))
-
-    // throw new ForbiddenError('Forbidden!');
-    // _.hasIn(req, '');
-
-    //auth stuff happens here
-    // if (!req || !req.headers) {
-    //   return;
-    // }
-    // const token = req.headers.authorization || '';
-    // const checkToken = await userController.findOrCreateUser(token);
-    // if (!checkToken.hasOwnProperty('authorized')) {
-    //   return { user: checkToken, authorized: true };
-    // }
-    // return checkToken;
-  }
+  context: ({ req }) => auth.context({ req })
 });
 
 module.exports = server;
